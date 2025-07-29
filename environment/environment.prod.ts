7e8ed7d9ee4ec7fbeee2e75eba56a227938c70ab
hnunezz <nunesdealmeidahenrique@gmail.com>
2025-07-29 15:21:02 -0300

    fix: env

--- environment/environment.prod.ts
+++ environment/environment.prod.ts
@@ -1,5 +1,5 @@
 // environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-whats-my-dream-mean.vercel.app/'
+  apiUrl: 'https://backend-whats-my-dream-mean-m2rge4k7w-hnunezzs-projects.vercel.app/'
 };
